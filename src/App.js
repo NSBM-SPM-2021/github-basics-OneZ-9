b4b7b51d38dd542551e5dae0c192574371624c77
Chamod Tharuka <chamodtharuka98@gmail.com>
2021-09-05 20:25:05 +0530

    Modifications for App.js file

--- src/App.js
+++ src/App.js
@@ -6,26 +6,23 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetupPage />
         </Route>
-
         <Route path="/new-meetup">
           <NewMeetupPage />
         </Route>
-
         <Route path="/favorites">
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
