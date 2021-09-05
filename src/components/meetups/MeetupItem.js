d43ce9b26339285b4bb07500145146dacde375b4
Chamod Tharuka <chamodtharuka98@gmail.com>
2021-09-05 15:10:43 +0530

    Modification to MeetupItem.js file

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,19 @@
-import classes from "./MeetupItem.module.css";
+import classess from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
-    <li className={classes.item}>
-      <dev className={classes.image}>
+    <li className={classess.item}>
+      <div className={classess.image}>
         <img src={props.image} alt={props.title} />
-      </dev>
-      <dev className={classes.content}>
+      </div>
+      <div className={classess.content}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
         <p>{props.description}</p>
-      </dev>
-      <dev className={classes.actions}>
+      </div>
+      <div className={classess.actions}>
         <button>To Favorites</button>
-      </dev>
+      </div>
     </li>
   );
 }
