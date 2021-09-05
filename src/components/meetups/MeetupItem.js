f268d136c23616dbd25fff854149e198e08d2380
Chamod Tharuka <chamodtharuka98@gmail.com>
2021-09-05 15:55:24 +0530

    Modification for MeetupItem.js file

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,22 @@
+import Card from "../ui/Card";
 import classess from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={classess.item}>
-      <div className={classess.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classess.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classess.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classess.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classess.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classess.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
